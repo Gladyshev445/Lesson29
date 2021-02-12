d1928023d5f3e3717f7aef407acd9c47d7e5961b
Gladyshev445 <74065918+Gladyshev445@users.noreply.github.com>
2021-02-12 18:51:43 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+//let x =13
+//   y =126
+  //  z =136
+    //c = x+y+z
+   // console.log(c)
+    //h = z+y*x
+    //console.log(h)
+    //o = z-y*x
+    //console.log(o)
+    //l = y+z+x
+    //console.log(l)
+    let x = prompt("Как вас зовут?")
+    alert ("Здраствуйте:" + x)
+    console.log(x)
+    let y = prompt("Сколько вам лет?")
+    console.log(y)
+    let z = prompt("Кем вы работаете?")
+    console.log(z)
+    let t = prompt("Ваш IQ?")
+    console.log(t)
+    let v = prompt("Вы робот?")
+    console.log(v)
